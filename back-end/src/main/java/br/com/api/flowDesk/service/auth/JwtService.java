b24ee9977f0e56d380e602994cd2ae5df19615e5
Dev-Isaque <devisaquesoares@gmail.com>
2026-04-14 14:36:51 -0300

    .

---
+++ back-end/src/main/java/br/com/api/flowDesk/service/auth/JwtService.java
@@ -0,0 +1,47 @@
+package br.com.api.flowDesk.service.auth;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Service
+public class JwtService {
+
+    private final String SECRET_KEY = "sua_chave_secreta_muito_forte";
+
+    private final long EXPIRATION = 1000 * 60 * 60 * 24;
+
+    public String generateToken(Long userId) {
+        return Jwts.builder()
+                .setSubject(String.valueOf(userId))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public Long getUserId(String token) {
+        Claims claims = getClaims(token);
+        return Long.parseLong(claims.getSubject());
+    }
+
+    public boolean isValid(String token) {
+        try {
+            getClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
